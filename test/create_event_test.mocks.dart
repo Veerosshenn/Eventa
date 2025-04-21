34a1f790a760dd8eeb3e5ef8717677dc3963041d
Veerosshenn <107527159+Veerosshenn@users.noreply.github.com>
2025-04-22 02:18:00 +0800

    Update to the Mocks

--- test/create_event_test.mocks.dart
+++ test/create_event_test.mocks.dart
@@ -1009,7 +1009,6 @@ class MockTaskSnapshot extends _i1.Mock implements _i6.TaskSnapshot {
 /// A class which mocks [DocumentReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockDocumentReference<T extends Object?> extends _i1.Mock
     implements _i4.DocumentReference<T> {
   MockDocumentReference() {
@@ -1145,7 +1144,6 @@ class MockDocumentReference<T extends Object?> extends _i1.Mock
 /// A class which mocks [CollectionReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockCollectionReference<T extends Object?> extends _i1.Mock
     implements _i4.CollectionReference<T> {
   MockCollectionReference() {
--- test/register_organizer_test.mocks.dart
+++ test/register_organizer_test.mocks.dart
@@ -1239,7 +1239,6 @@ class MockUser extends _i1.Mock implements _i4.User {
 /// A class which mocks [DocumentReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockDocumentReference<T extends Object?> extends _i1.Mock
     implements _i6.DocumentReference<T> {
   MockDocumentReference() {
@@ -1375,7 +1374,6 @@ class MockDocumentReference<T extends Object?> extends _i1.Mock
 /// A class which mocks [CollectionReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockCollectionReference<T extends Object?> extends _i1.Mock
     implements _i6.CollectionReference<T> {
   MockCollectionReference() {
